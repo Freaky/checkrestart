c9016467e18619298a53eda4d29d563262c9601a
Thomas Hurst <tom@hur.st>
2020-02-18 00:23:44 +0000

    Detect missing libraries

--- checkrestart.c
+++ checkrestart.c
@@ -12,25 +12,24 @@
 #include <string.h>
 
 static int
-getpathname(pid_t pid, char *pathname, size_t maxlen) {
+getprocstr(pid_t pid, int node, char *str, size_t maxlen) {
 	size_t len = maxlen;
-	int name[4] = { CTL_KERN, KERN_PROC, KERN_PROC_PATHNAME, pid };
-	pathname[0] = '\0';
-	int error = sysctl(name, nitems(name), pathname, &len, NULL, 0);
+	int name[4] = { CTL_KERN, KERN_PROC, node, pid };
+	str[0] = '\0';
+	int error = sysctl(name, nitems(name), str, &len, NULL, 0);
 	if (error != 0) return errno;
 	else return 0;
 }
 
 static int
-getargs(pid_t pid, char *args, size_t maxlen) {
-	size_t len = maxlen;
-	int name[4] = { CTL_KERN, KERN_PROC, KERN_PROC_ARGS, pid };
-	args[0] = '\0';
-	int error = sysctl(name, nitems(name), args, &len, NULL, 0);
-	if (error != 0) return errno;
-	else return 0;
+getpathname(pid_t pid, char *pathname, size_t maxlen) {
+	return getprocstr(pid, KERN_PROC_PATHNAME, pathname, maxlen);
 }
 
+static int
+getargs(pid_t pid, char *args, size_t maxlen) {
+	return getprocstr(pid, KERN_PROC_ARGS, args, maxlen);
+}
 
 int main(void) {
 	struct procstat *prstat = procstat_open_sysctl();
@@ -59,10 +58,30 @@ int main(void) {
 			(void)getargs(proc->ki_pid, args, sizeof(args)); // strictly best effort
 			// missing executable, immediate restart candidate
 			// search plists for files with the name of this executable as potential candidates
-			printf("%05d\t%16s\t[MISSING EXECUTABLE]\t%s\n", proc->ki_pid, proc->ki_comm, args);
+			printf("%05d\t%16s\t[MISSING EXE]\t%s\n", proc->ki_pid, proc->ki_comm, args);
 		} else {
 			// get vm map, find executable vn mappings with no associated file
 			// or with file not in plist
+			unsigned int vmcnt;
+			struct kinfo_vmentry *freep = procstat_getvmmap(prstat, proc, &vmcnt);
+
+			int missing = 0;
+
+			for (unsigned y = 0; y < vmcnt; y++) {
+				struct kinfo_vmentry *kve = &freep[y];
+				if ((kve->kve_protection & KVME_PROT_EXEC) == KVME_PROT_EXEC &&
+				    kve->kve_type == KVME_TYPE_VNODE &&
+				    strlen(kve->kve_path) == 0) {
+					missing = 1;
+					break;
+				}
+			}
+
+			procstat_freevmmap(prstat, freep);
+
+			if (missing) {
+				printf("%05d\t%16s\t[MISSING LIB]\t%s\n", proc->ki_pid, proc->ki_comm, pathname);
+			}
 		}
 	}
 
