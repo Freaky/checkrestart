0339c7d93aac30e4477a8c24d4699c58d2a36a6c
Thomas Hurst <tom@hur.st>
2020-02-20 17:52:45 +0000

    Pick a few style nits

--- checkrestart.c
+++ checkrestart.c
@@ -18,31 +18,37 @@ static int needheader = 1;
 static int binonly = 0;
 
 static int
-getprocstr(pid_t pid, int node, char *str, size_t maxlen) {
+getprocstr(pid_t pid, int node, char *str, size_t maxlen)
+{
 	size_t len = maxlen;
 	int name[4] = { CTL_KERN, KERN_PROC, node, pid };
 	str[0] = '\0';
 	int error = sysctl(name, nitems(name), str, &len, NULL, 0);
-	if (error != 0)
-	{
-		if (errno == ENOMEM) str[len] = '\0';
-		else return errno;
+	if (error != 0) {
+		if (errno == ENOMEM) {
+			str[len] = '\0';
+		} else {
+			return (errno);
+		}
 	}
-	return 0;
+	return (0);
 }
 
 static int
-getpathname(pid_t pid, char *pathname, size_t maxlen) {
-	return getprocstr(pid, KERN_PROC_PATHNAME, pathname, maxlen);
+getpathname(pid_t pid, char *pathname, size_t maxlen)
+{
+	return (getprocstr(pid, KERN_PROC_PATHNAME, pathname, maxlen));
 }
 
 static int
-getargs(pid_t pid, char *args, size_t maxlen) {
-	return getprocstr(pid, KERN_PROC_ARGS, args, maxlen);
+getargs(pid_t pid, char *args, size_t maxlen)
+{
+	return (getprocstr(pid, KERN_PROC_ARGS, args, maxlen));
 }
 
 static void
-needsrestart(const struct kinfo_proc *proc, const char *why, const char *note) {
+needsrestart(const struct kinfo_proc *proc, const char *why, const char *note)
+{
 	if (needheader) {
 		needheader = 0;
 		printf("%5s %5s %16s %7s %s\n", "PID", "JID", "PROCESS", "UPDATED", "COMMAND");
@@ -51,23 +57,32 @@ needsrestart(const struct kinfo_proc *proc, const char *why, const char *note) {
 }
 
 static void
-usage(void) {
+usage(void)
+{
 	printf("usage: checkrestart [-Hb] [pid [pid ...]]\n");
 	exit(1);
 }
 
 static void
-checkrestart(struct procstat *prstat, struct kinfo_proc *proc) {
+checkrestart(struct procstat *prstat, struct kinfo_proc *proc)
+{
 	char pathname[PATH_MAX];
 
 	// Skip kernel processes
-	if (proc->ki_ppid == 0) return;
+	if (proc->ki_ppid == 0) {
+		return;
+	}
 
 	int error = getpathname(proc->ki_pid, pathname, sizeof(pathname));
-	if (error != 0 && error != ENOENT) return;
+	if (error != 0 && error != ENOENT) {
+		return;
+	}
+
 	if (error == ENOENT) {
 		// Verify ENOENT isn't down to the process going away
-		if (kill(proc->ki_pid, 0) == -1 && errno == ESRCH) return;
+		if (kill(proc->ki_pid, 0) == -1 && errno == ESRCH) {
+			return;
+		}
 
 		// Binary path is just empty. Get its argv instead
 		char args[PATH_MAX];
@@ -80,6 +95,7 @@ checkrestart(struct procstat *prstat, struct kinfo_proc *proc) {
 		// Find executable vnode-backed mappings, usually indicating a shared library
 		for (unsigned int j = 0; j < vmcnt; j++) {
 			struct kinfo_vmentry *kve = &vmaps[j];
+
 			if ((kve->kve_protection & KVME_PROT_EXEC) == KVME_PROT_EXEC &&
 			    kve->kve_type == KVME_TYPE_VNODE && !*kve->kve_path) {
 				needsrestart(proc, "Library", pathname);
@@ -92,26 +108,29 @@ checkrestart(struct procstat *prstat, struct kinfo_proc *proc) {
 }
 
 int
-main(int argc, char **argv) {
+main(int argc, char **argv)
+{
 	char ch;
+
 	while ((ch = getopt(argc, argv, "Hb")) != -1)
 		switch (ch) {
-			case 'H':
-				needheader = 0;
-				break;
-			case 'b':
-				binonly = 1;
-				break;
-			case '?':
-			default:
-				usage();
+		case 'H':
+			needheader = 0;
+			break;
+		case 'b':
+			binonly = 1;
+			break;
+		case '?':
+		default:
+			usage();
 		}
-
 	argc -= optind;
 	argv += optind;
 
 	struct procstat *prstat = procstat_open_sysctl();
-	if (prstat == NULL) errx(1, "procstat_open()");
+	if (prstat == NULL) {
+		errx(1, "procstat_open()");
+	}
 
 	unsigned int cnt;
 
@@ -120,11 +139,19 @@ main(int argc, char **argv) {
 		while (argc--) {
 			char *end;
 			pid_t pid = strtoimax(*argv, &end, 10);
-			if (*end != '\0') usage();
+
+			if (*end != '\0') {
+				usage();
+			}
+
 			struct kinfo_proc *p = procstat_getprocs(prstat, KERN_PROC_PID, pid, &cnt);
-			if (p == NULL) warn("procstat_getprocs(%d)", pid);
-			else {
-				if (cnt == 1) checkrestart(prstat, p);
+
+			if (p == NULL) {
+				warn("procstat_getprocs(%d)", pid);
+			} else {
+				if (cnt == 1) {
+					checkrestart(prstat, p);
+				}
 				procstat_freeprocs(prstat, p);
 			}
 			argv++;
@@ -132,7 +159,9 @@ main(int argc, char **argv) {
 	} else {
 		// all processes
 		struct kinfo_proc *p = procstat_getprocs(prstat, KERN_PROC_PROC, 0, &cnt);
-		if (p == NULL) errx(1, "procstat_getprocs()");
+		if (p == NULL) {
+			errx(1, "procstat_getprocs()");
+		}
 
 		for (unsigned int i = 0; i < cnt; i++) {
 			checkrestart(prstat, &p[i]);
@@ -142,5 +171,5 @@ main(int argc, char **argv) {
 	}
 
 	procstat_close(prstat);
-	return 0;
+	return (0);
 }
